68af976e13d14288506832ab2d2bd7288e68879e
Apolinar01 <LuisAnge2022@outlook.es>
2022-02-21 21:56:28 -0600

    otro cambio nuevo

--- js/validacion.js
+++ js/validacion.js
@@ -1,15 +1,50 @@
-eventListener()
-  function eventListener(){
-    const formulario =document.querySelector('#formulario');
-    formulario.addEventListener('submit',validar)
-    
+eventListener();
+function eventListener() {
+  const formulario = document.querySelector("#formulario");
+  formulario.addEventListener("submit", validar);
 }
-function validar(e){
-    e.preventDefault();
-    let nombre =document.getElementById('nombre').value;
- 
-    if (nombre=='') {
-      swal.fire('No puede dejar campos vacios')
-    }
+function validar(e) {
+  e.preventDefault();
+  let nombre = document.getElementById("nombre").value;
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
 
+  if (nombre == "") {
+    swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debes de llenar el campo nombre",
+    });
+  } else if (email == "") {
+    swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debes de llenar el campo email",
+    });
+  } else if (subject == "") {
+    swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debes de llenar el campo asunto",
+    });
+  } else if (message == "") {
+    swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debes de llenar el campo mensaje",
+    });
+  } else if (
+    (nombre == nombre) &
+    (email == email) &
+    (subject == subject) &
+    (message == message)
+  ) {
+    swal.fire({
+      icon: "success",
+      title: "El correo fue enviado de manera exitosa",
+      showConfirmButton: false,
+      timer: 1600,
+    });
   }
+}
